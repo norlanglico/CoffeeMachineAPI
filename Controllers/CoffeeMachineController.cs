f1ca8c222273747ae35d767bb5882f995d21fe5d
Norlan Glico <norlan.glico15@gmail.com>
2024-05-12 02:21:10 +1200

    Implement a GET method brew-coffee

---
+++ Controllers/CoffeeMachineController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeMachineAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CoffeeMachineController : ControllerBase
+    {
+        [HttpGet("brew-coffee")]
+        public IActionResult Get()
+        {
+            var response = new
+            {
+                message = "Your piping hot coffee is ready",
+                prepared = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:sszzz")
+            };
+
+            return Ok(response);
+        }    
+    }
+}
